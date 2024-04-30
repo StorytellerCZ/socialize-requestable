9096ba3a375bf31790209561f1971a8d7297a1a4
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-04-30 18:28:37 +0200

    Remove indexes

--- common/request-model.js
+++ common/request-model.js
@@ -2,52 +2,68 @@
 import SimpleSchema from 'meteor/aldeed:simple-schema'
 /* eslint-disable import/no-unresolved */
 
-export default ({ Meteor, Mongo, BaseModel, LinkableModel, LinkParent, ServerTime }) => {
-    const RequestsCollection = new Mongo.Collection('socialize:requests');
+export default ({
+  Meteor,
+  Mongo,
+  BaseModel,
+  LinkableModel,
+  LinkParent,
+  ServerTime
+}) => {
+  const RequestsCollection = new Mongo.Collection('socialize:requests')
 
-    if (RequestsCollection.configureRedisOplog) {
-        RequestsCollection.configureRedisOplog({
-            mutation(options, { selector, doc }) {
-                const namespaces = [];
-                if (doc) {
-                    namespaces.push(doc.linkedObjectId, doc.requesterId);
-                } else if (selector) {
-                    const { _id, linkedObjectId, requesterId } = selector;
+  if (RequestsCollection.configureRedisOplog) {
+    RequestsCollection.configureRedisOplog({
+      mutation(options, { selector, doc }) {
+        const namespaces = []
+        if (doc) {
+          namespaces.push(doc.linkedObjectId, doc.requesterId)
+        } else if (selector) {
+          const { _id, linkedObjectId, requesterId } = selector
 
-                    if (_id) {
-                        const request = RequestsCollection.findOne({ _id: selector._id }, { fields: { linkedObjectId: 1, requesterId: 1 } });
-                        if (request) {
-                            namespaces.push(request.linkedObjectId, request.requesterId);
-                        }
-                    } else {
-                        linkedObjectId && namespaces.push(linkedObjectId);
-                        requesterId && namespaces.push(requesterId);
-                    }
+          if (_id) {
+            const request = RequestsCollection.findOne(
+              { _id: selector._id },
+              {
+                fields: {
+                  linkedObjectId: 1,
+                  requesterId: 1
                 }
-                Object.assign(options, {
-                    namespaces,
-                });
-            },
-            cursor(options, selector) {
-                const newSelector = (selector.$or && selector.$or[0]) || selector;
-                const selectorId = newSelector.linkedObjectId || newSelector.requesterId;
-                if (selectorId) {
-                    Object.assign(options, {
-                        namespace: selectorId,
-                    });
-                }
-            },
-        });
-    }
+              }
+            )
+            if (request) {
+              namespaces.push(request.linkedObjectId, request.requesterId)
+            }
+          } else {
+            linkedObjectId && namespaces.push(linkedObjectId)
+            requesterId && namespaces.push(requesterId)
+          }
+        }
+        Object.assign(options, {
+          namespaces
+        })
+      },
+      cursor(options, selector) {
+        const newSelector = (selector.$or && selector.$or[0]) || selector
+        const selectorId = newSelector.linkedObjectId || newSelector.requesterId
+        if (selectorId) {
+          Object.assign(options, {
+            namespace: selectorId
+          })
+        }
+      }
+    })
+  }
 
-    const acceptHooks = {};
-    let requestTypes = [];
-    /**
+  const acceptHooks = {}
+  let requestTypes = []
+
+  /**
    * The Request Class
    * @class Request
    * @param {Object} document An object representing a request, usually a Mongo document
    */
-    class Request extends LinkableModel(BaseModel) {
+  class Request extends LinkableModel(BaseModel) {
     /**
      * onAccepted - Register a function to run when the request is accepted
      *
@@ -57,153 +73,165 @@ export default ({ Meteor, Mongo, BaseModel, LinkableModel, LinkParent, ServerTim
      * @throws {Meteor.error}
      */
 
-        static onAccepted(Model, acceptedHook) {
-            if (new Model() instanceof LinkParent) {
-                if (typeof acceptedHook === 'function') {
-                    const hookName = Model.prototype.getCollectionName();
+    static onAccepted(Model, acceptedHook) {
+      if (new Model() instanceof LinkParent) {
+        if (typeof acceptedHook === 'function') {
+          const hookName = Model.prototype.getCollectionName()
 
-                    if (!acceptHooks[hookName]) {
-                        acceptHooks[hookName] = [];
-                    }
+          if (!acceptHooks[hookName]) {
+            acceptHooks[hookName] = []
+          }
 
-                    acceptHooks[hookName].push(acceptedHook);
-                } else {
-                    throw new Meteor.Error('notAFunction', 'Second parameter of onAccepted must be a function');
-                }
-            } else {
-                throw new Meteor.Error('notALinkParent', 'First parameter of onAccepted must be a model of type LinkParent');
-            }
+          acceptHooks[hookName].push(acceptedHook)
+        } else {
+          throw new Meteor.Error(
+            'notAFunction',
+            'Second parameter of onAccepted must be a function'
+          )
         }
+      } else {
+        throw new Meteor.Error(
+          'notALinkParent',
+          'First parameter of onAccepted must be a model of type LinkParent'
+        )
+      }
+    }
 
-        static registerRequestType(type) {
-            // create a set from the requestTypes array so we only add unique values
-            const typesSet = new Set(requestTypes);
+    static registerRequestType(type) {
+      // create a set from the requestTypes array so we only add unique values
+      const typesSet = new Set(requestTypes)
 
-            typesSet.add(type);
+      typesSet.add(type)
 
-            requestTypes = Array.from(typesSet);
-        }
+      requestTypes = Array.from(typesSet)
+    }
 
-        /**
+    /**
      * Get the User instance for the user who made the request
      * @returns {User} The user who made the request
      */
-        requester() {
-            return Meteor.users.findOne({ _id: this.requesterId });
-        }
+    requester() {
+      return Meteor.users.findOne({ _id: this.requesterId })
+    }
 
-        /**
+    /**
      * Get the User instance for the user who is invited if exists
      * @returns {User} The user who is invited
      */
-        invited() {
-            if (this.invited) {
-                return Meteor.users.findOne({ _id: this.invited });
-            }
-            return null;
-        }
+    invited() {
+      if (this.invited) {
+        return Meteor.users.findOne({ _id: this.invited })
+      }
+      return null
+    }
 
-        /**
+    /**
      * Accept the request
      */
-        accept() {
-            acceptHooks[this.objectType].forEach((hook) => {
-                hook.call(this);
-            });
-            this.update({ $set: { acceptedAt: ServerTime.date() } })
-        }
+    accept() {
+      acceptHooks[this.objectType].forEach((hook) => {
+        hook.call(this)
+      })
+      this.update({ $set: { acceptedAt: ServerTime.date() } })
+    }
 
-        /**
+    /**
      * Deny the request
      */
-        deny() {
-            this.update({ $set: { deniedAt: ServerTime.date() } });
-        }
+    deny() {
+      this.update({ $set: { deniedAt: ServerTime.date() } })
+    }
 
-        /**
+    /**
      * Ignore the request so that it can be accepted or deniedAt later
      */
-        ignore() {
-            this.update({ $set: { ignoredAt: ServerTime.date() } });
-        }
+    ignore() {
+      this.update({ $set: { ignoredAt: ServerTime.date() } })
+    }
 
-        /**
+    /**
      * Cancel the request
      */
-        cancel() {
-            this.remove();
-        }
+    cancel() {
+      this.remove()
+    }
 
-        /**
+    /**
      * Check if the request had been responded to
      * @returns {Boolean} Whether the request has been responded to
      */
-        wasRespondedTo() {
-            return !!this.deniedAt || !!this.ignoredAt;
-        }
+    wasRespondedTo() {
+      return !!this.deniedAt || !!this.ignoredAt
     }
+  }
 
-    Request.attachCollection(RequestsCollection);
+  Request.attachCollection(RequestsCollection)
 
+  // Create the schema for a request
+  RequestsCollection.attachSchema(
+    new SimpleSchema({
+      requesterId: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id,
+        autoValue() {
+          if (this.isInsert) {
+            return this.userId
+          }
+          return undefined
+        },
+        denyUpdate: true
+      },
+      invitedId: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id,
+        denyUpdate: true,
+        optional: true
+      },
+      type: {
+        type: String,
+        allowedValues: () => requestTypes
+      },
+      createdAt: {
+        type: Date,
+        autoValue() {
+          if (this.isInsert) {
+            return ServerTime.date()
+          }
+          return undefined
+        },
+        denyUpdate: true
+      },
+      deniedAt: {
+        type: Date,
+        optional: true
+      },
+      ignoredAt: {
+        type: Date,
+        optional: true
+      },
+      acceptedAt: {
+        type: Date,
+        optional: true
+      },
+      data: {
+        type: Object,
+        blackbox: true,
+        optional: true
+      }
+    })
+  )
 
-    // Create the schema for a request
-    RequestsCollection.attachSchema(new SimpleSchema({
-            requesterId: {
-                type: String,
-                regEx: SimpleSchema.RegEx.Id,
-                autoValue() {
-                    if (this.isInsert) {
-                        return this.userId;
-                    }
-                    return undefined;
-                },
-                index: 1,
-                denyUpdate: true,
-            },
-            invitedId: {
-                type: String,
-                regEx: SimpleSchema.RegEx.Id,
-                index: 1,
-                denyUpdate: true,
-                optional: true,
-            },
-            type: {
-                type: String,
-                allowedValues: () => requestTypes,
-            },
-            createdAt: {
-                type: Date,
-                autoValue() {
-                    if (this.isInsert) {
-                        return ServerTime.date();
-                    }
-                    return undefined;
-                },
-                index: -1,
-                denyUpdate: true,
-            },
-            deniedAt: {
-                type: Date,
-                optional: true,
-            },
-            ignoredAt: {
-                type: Date,
-                optional: true,
-            },
-            acceptedAt: {
-                type: Date,
-                optional: true
-            },
-            data: {
-                type: Object,
-                blackbox: true,
-                optional: true,
-            },
-        })
-    );
+  if (Meteor.isServer) {
+    RequestsCollection.createIndexAsync({
+      requesterId: 1,
+      invitedId: 1,
+      type: 1
+    })
+    RequestsCollection.createIndexAsync({ createdAt: -1 })
+  }
 
-    // add the LinkableSchema to the request class
-    Request.appendSchema(LinkableModel.LinkableSchema);
+  // add the LinkableSchema to the request class
+  Request.appendSchema(LinkableModel.LinkableSchema)
 
-    return { Request, RequestsCollection };
-};
+  return { Request, RequestsCollection }
+}
--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 /* global Package */
 Package.describe({
   name: 'socialize:requestable',
-  version: '1.0.6',
+  version: '1.1.0',
   summary: 'Create models that are requestable',
   git: 'https://github.com/copleykj/socialize-requestable.git'
 })
 
 Package.onUse(function _(api) {
-  api.versionsFrom(['1.10.2', '2.3', '3.0-beta.0'])
+  api.versionsFrom(['1.10.2', '2.3', '3.0-rc.0'])
 
   api.use('socialize:linkable-model@1.0.6')
 
