245a60fd89c763d545cb968b5000212eab61c19d
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-05-05 00:29:45 +0200

    Add simple-schema dependency

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* global Package */
 Package.describe({
   name: 'socialize:requestable',
-  version: '1.1.0',
+  version: '2.0.0',
   summary: 'Create models that are requestable',
   git: 'https://github.com/copleykj/socialize-requestable.git'
 })
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function _(api) {
   api.versionsFrom(['1.10.2', '2.3', '3.0-rc.0'])
 
-  api.use('socialize:linkable-model@1.0.6')
+  api.use(['socialize:linkable-model@2.0.0', 'aldeed:simple-schema@1.13.1'])
 
   api.mainModule('common/common.js')
 })
