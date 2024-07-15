1bfff2a00746a4d1f0316f15f155f91095c82faf
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-07-15 21:17:00 +0200

    Meteor 3 version updates

--- package.js
+++ package.js
@@ -7,9 +7,9 @@ Package.describe({
 })
 
 Package.onUse(function _(api) {
-  api.versionsFrom(['1.10.2', '2.3', '3.0-rc.0'])
+  api.versionsFrom(['1.10.2', '2.3', '3.0'])
 
-  api.use(['socialize:linkable-model@2.0.0', 'aldeed:simple-schema@1.13.1'])
+  api.use(['socialize:linkable-model@2.0.0', 'aldeed:simple-schema@1.13.1 || 2.0.0'])
 
   api.mainModule('common/common.js')
 })
