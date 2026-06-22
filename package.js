4cc61261b8cdf5d2f5e96c68834d06ae8f7024ef
Jan Dvorak <jdvorak@literaryuniverse.com>
2026-06-22 12:51:38 +0900

    simple-schema v3

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function _(api) {
   api.versionsFrom(['1.10.2', '2.3', '3.0'])
 
-  api.use(['socialize:linkable-model@2.0.0', 'aldeed:simple-schema@1.13.1 || 2.0.0'])
+  api.use(['socialize:linkable-model@2.0.0', 'aldeed:simple-schema@2.0.0 || 3.0.0'])
 
   api.mainModule('common/common.js')
 })
